ecd295c85b2d41e731d45cb579eba5f719eddd15
jedrzejkozal <jedrzej.kozal@gmail.com>
2019-02-02 12:37:28 +0100

    day10 2017 update

--- 2017/day10/main.py
+++ 2017/day10/main.py
@@ -1,7 +1,14 @@
 from KnotHash import *
+from MultipleRounds import *
 
 list = list(range(256))
 input_length = [46,41,212,83,1,255,157,65,139,52,39,254,2,86,0,204]
 k = KnotHash(list, input_length)
 k.knot_list()
-print(k.list[0] * k.list[1])
+print("fist half", k.list[0] * k.list[1])
+
+
+new_input = "46,41,212,83,1,255,157,65,139,52,39,254,2,86,0,204"
+mr = MultipleRounds()
+result = mr.calc_hash(list, new_input)
+print("second half", result)
